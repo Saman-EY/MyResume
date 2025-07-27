6b6b20281046ee87d6e9251304ce6b9771469612
Saman <samanezzatabadi@yahoo.com>
2025-07-27 22:55:06 +0330

    temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
         height={400}
         className="w-fit  object-contain absolute  right-0 -z-10"
       />
+      
       {/* <Nav /> */}
       <Profile />
       <Skills />
